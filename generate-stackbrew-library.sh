3ec33af60201ad026d16d65581bee55ed10f5c70
Carlos Sanchez <carlos@apache.org>
2016-04-25 13:47:05 +0200

    Add alpine branch

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -3,16 +3,20 @@ set -e
 
 latest='jdk-8'
 
-cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-versions=( jdk-*/ )
-versions=( "${versions[@]%/}" )
 url='git://github.com/carlossg/docker-maven'
 
-echo '# maintainer: Carlos Sanchez <carlos@apache.org> (@carlossg)'
+generate-version() {
+	local version=$1
+	local branch=$2
+	local branch_suffix=""
 
-for version in "${versions[@]}"; do
-	commit="$(git log -1 --format='format:%H' -- "$version")"
+	if [ "$branch" != 'master' ]; then
+		branch_suffix="-${branch}"
+	fi
+
+	commit="$(git log -1 --format='format:%H' "$branch" -- "$version")"
 	
 	mavenVersion="$(grep -m1 'ENV MAVEN_VERSION ' "$version/Dockerfile" | cut -d' ' -f3)"
 	
@@ -31,19 +35,32 @@ for version in "${versions[@]}"; do
 	
 	echo
 	for va in "${versionAliases[@]}"; do
-		echo "$va: ${url}@${commit} $version"
+		echo "${va}${branch_suffix}: ${url}@${commit} $version"
 	done
 	
 	for variant in onbuild; do
-		commit="$(git log -1 --format='format:%H' -- "$version/$variant")"
+		commit="$(git log -1 --format='format:%H' "$branch" -- "$version/$variant")"
 		echo
 		for va in "${versionAliases[@]}"; do
 			if [ "$va" = 'latest' ]; then
-				va="$variant"
+				va="${variant}${branch_suffix}"
 			else
-				va="$va-$variant"
+				va="$va-${variant}${branch_suffix}"
 			fi
 			echo "$va: ${url}@${commit} $version/$variant"
 		done
+	done
+}
+
+echo '# maintainer: Carlos Sanchez <carlos@apache.org> (@carlossg)'
+
+versions=( jdk-*/ )
+versions=( "${versions[@]%/}" )
+
+for version in "${versions[@]}"; do
+	for branch in master alpine; do
+		if ! ( [ "$version" == 'jdk-9' ] && [ "$branch" == 'alpine' ] ); then # no base image for jdk-9-alpine yet
+			generate-version "$version" "$branch"
+		fi
 	done
 done
