bdcedecba28295e7fc91256c9e58b0cc1f69ed57
Carlos Sanchez <carlos@apache.org>
2017-09-09 20:27:32 +0200

    Move alpine dockerfiles to master

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
-latest='jdk-8'
+latest='8'
+default_jdk=jdk
+variants=( alpine slim )
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
@@ -23,15 +25,45 @@ generate-version() {
 	versionAliases=()
 	while [ "${mavenVersion%[.-]*}" != "$mavenVersion" ]; do
 		versionAliases+=( $mavenVersion-$version )
-		if [ "$version" = "$latest" ]; then
+		# tag 3.5, 3.5.0
+		if [[ "$version" == *"$default_jdk-$latest" ]]; then
 			versionAliases+=( $mavenVersion )
+		elif [[ "$version" == *"-$latest" ]]; then
+			# tag 3-ibmjava, 3.5-ibmjava, 3.5.0-ibmjava
+			versionAliases+=( $mavenVersion-${version//-$latest/} )
 		fi
+		# tag 3.5-alpine, 3.5.0-alpine, 3.5-slim, 3.5.0-slim
+		for variant in "${variants[@]}"; do
+			if [[ "$version" == "$default_jdk-$latest-$variant" ]]; then
+				versionAliases+=( $mavenVersion-$variant )
+			elif [[ "$version" == *"-$latest-$variant" ]]; then
+				versionAliases+=( $mavenVersion-${version//-$latest/} )
+			fi
+		done
 		mavenVersion="${mavenVersion%[.-]*}"
 	done
+
+	# tag full version
 	versionAliases+=( $mavenVersion-$version )
-	if [ "$version" = "$latest" ]; then
+
+	# tag 3, latest
+	if [[ "$version" == "$default_jdk-$latest" ]]; then
 		versionAliases+=( $mavenVersion latest )
+	elif [[ "$version" == *"-$latest" ]]; then
+		# tag 3-ibmjava ibmjava
+		versionAliases+=( $mavenVersion-${version//-$latest/} ${version//-$latest/} )
 	fi
+
+	# tag alpine, slim
+	for variant in "${variants[@]}"; do
+		if [[ "$version" == *"$latest-$variant" ]]; then
+			if [[ "$version" == "$default_jdk-$latest"* ]]; then
+				versionAliases+=( ${version//$default_jdk-$latest-/} )
+			else
+				versionAliases+=( ${version//-$latest/} )
+			fi
+		fi
+	done
 	
 	echo
 	for va in "${versionAliases[@]}"; do
@@ -49,9 +81,6 @@ versions=( jdk-*/ ibmjava-*/ )
 versions=( "${versions[@]%/}" )
 
 for version in "${versions[@]}"; do
-	for branch in master slim alpine; do
-		if ! ( [[ "$version" == "jdk-9" ]] && [ "$branch" == 'alpine' ] ); then # no base image for openjdk-9-alpine yet
-			generate-version "$version" "$branch"
-		fi
-	done
+	branch=master
+	generate-version "$version" "$branch"
 done
