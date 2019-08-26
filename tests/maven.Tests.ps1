dc81bc7603335452706ca7d9ab60a495d1c3b1dd
Alex Earl <slide.o.mix@gmail.com>
2019-08-26 16:36:43 -0700

    Fix test

--- tests/maven.Tests.ps1
+++ tests/maven.Tests.ps1
@@ -67,7 +67,7 @@ Describe "$SUT_TAG repository is created" {
 
 Describe "$SUT_TAG run Maven" {
   It 'runs maven' {
-    $exitCode, $stdout, $stderr = Run-Program -Cmd "docker.exe" -Params "run --rm $SUT_TEST_IMAGE mvn -B -ntp -f $env:TEMP install"
+    $exitCode, $stdout, $stderr = Run-Program -Cmd "docker.exe" -Params "run --rm $SUT_TEST_IMAGE mvn -B -ntp -f C:/Temp install"
     $exitCode | Should -Be 0
   }
 }
