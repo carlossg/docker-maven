6f912331309f5ab9745a91ef5884f3a0b46dfc1d
Jiayi Zhao <jeff.no.zhao@gmail.com>
2019-10-31 11:25:00 -0400

    add dockerfiles for openj9
     - jdk8 and jdk11

--- publish.sh
+++ publish.sh
@@ -2,6 +2,7 @@
 
 for dir in jdk-11 jdk-12 jdk-13 jdk-14 \
   jdk-8-slim jdk-11-slim \
+  jdk-8-openj9 jdk-11-openj9 \
   ibmjava-8 ibmjava-8-alpine \
   amazoncorretto-8 azulzulu-11; do
     /bin/cp jdk-8/mvn-entrypoint.sh $dir/mvn-entrypoint.sh
