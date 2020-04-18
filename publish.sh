5768b638a84739d0ad85dbcbe42018075550b109
rmie <17530606+rmie@users.noreply.github.com>
2020-04-18 11:42:35 +0200

    add adoptopenjdk-14-(slim|openj9) images (#153)

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
 for dir in jdk-11 jdk-14 openjdk-15 \
-  jdk-8-slim jdk-11-slim \
-  jdk-8-openj9 jdk-11-openj9 \
+  jdk-8-slim jdk-11-slim jdk-14-slim \
+  jdk-8-openj9 jdk-11-openj9 jdk-14-openj9 \
   ibmjava-8 ibmjava-8-alpine \
   amazoncorretto-8 azulzulu-11; do
     /bin/cp jdk-8/mvn-entrypoint.sh $dir/mvn-entrypoint.sh
