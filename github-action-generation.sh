2d3d579ac46640d204bde82ecc9a2812e298c197
Carlos Sanchez <carlos@apache.org>
2020-04-19 11:47:09 +0200

    chore: workflow for everything else that may be added in a PR

--- github-action-generation.sh
+++ github-action-generation.sh
@@ -12,3 +12,12 @@ for version in "${all_dirs[@]}"; do
         sed -e "s/DIRECTORY/$version/" .github/github-action-template.yaml > ".github/workflows/$version.yml"
     fi
 done
+
+# Create a workflow for everything else that may be added in a PR
+cp .github/github-action-template.yaml .github/workflows/other.yml
+for f in *; do
+    gsed -i "/DIRECTORY\/\*\*/i \    - '!${f}\/\*\*'" .github/workflows/other.yml
+done
+gsed -i "s/Docker Maven Image CI DIRECTORY/Docker Maven Image CI other/" .github/workflows/other.yml
+gsed -i "s/github-action.sh DIRECTORY/github-action.sh/" .github/workflows/other.yml
+gsed -i "/DIRECTORY\/\*\*/,+3 d" .github/workflows/other.yml
