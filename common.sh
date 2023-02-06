6391fbc1c13b99b69c7bbc72826b9f192e28ac51
Alexander Brandes <mc.cache@web.de>
2023-02-06 18:30:49 +0100

    Update to maven 3.9.0

--- common.sh
+++ common.sh
@@ -59,7 +59,7 @@ version-aliases() {
 			fi
 		done
 
-		# tag eclipse-temurin-8-alpine -> 3.8.7-eclipse-temurin-alpine
+		# tag eclipse-temurin-8-alpine -> 3.9.0-eclipse-temurin-alpine
 		if [ -n "${extra_tags[$version]:-}" ]; then
 			versionAliases+=("$mavenVersion-${extra_tags[$version]}")
 		fi
