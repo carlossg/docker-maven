e73c65c5fd3344979e739ef5286d4df4bf45c126
Carlos Sanchez <carlos@apache.org>
2023-02-14 14:29:47 +0100

    build: remove the 'other' workflow

--- github-action-generation.sh
+++ github-action-generation.sh
@@ -14,17 +14,3 @@ for version in "${all_dirs[@]}"; do
         sed -e "s/DIRECTORY/$version/" .github/github-action-template.yaml >".github/workflows/$version.yml"
     fi
 done
-
-# Create a workflow for everything else that may be added in a PR
-cp .github/github-action-template.yaml .github/workflows/other.yml
-# Preserve sort order
-LC_COLLATE=C
-for f in *; do
-    sed -i "/DIRECTORY\/\*\*/i \      - '!${f}\/\*\*'" .github/workflows/other.yml
-done
-sed -i \
-    -e "s/DIRECTORY/Other/" \
-    -e "/DIRECTORY\/\*\*/,+4 d" \
-    -e '/- "tests\/\*\*"/d' \
-    -e '/- .github\/workflows\/_template.yml/d' \
-    .github/workflows/other.yml
